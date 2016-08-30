54a7c06af5cd18dc5a5260edb3f32ce74ee477f4
Omurbek <omurbek@Omurbeks-MacBook-Pro.local>
2016-08-30 17:38:07 +0600

    third commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index]
+  def index
+    @posts = Post.paginate(:page => params[:page], :per_page => 10).order('id DESC') 
+  end
+  def current_user?(user)
+    user ==current_user
+    
+  end
+  def correct_user
+    @user=User.find(params[:user_id])
+    redirect_to(root_url) unless current_user?(@user)
+    
+  end
+  def my_post
+    
+    @user=User.find(params[:user_id])
+    @posts = @user.posts.paginate(:page => params[:page], :per_page => 10).order('id DESC')
+    @post=Post.new
+    
+  end
+
+	def new
+    @user=User.find(params[:user_id])
+    @post = Post.new
+  	end
+
+	def create  
+  		@post = Post.create(post_params)
+  		redirect_to user_posts_path
+	end 
+
+	private
+
+	def post_params
+		params.require(:post).permit(:image, :description, :user_id)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
+
+  devise_for :users,:path=>'accounts'
+  root 'posts#index'
+  resources :users do
+    get 'my_post' => 'posts#my_post', as: 'my_post'
+    resources :posts
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
